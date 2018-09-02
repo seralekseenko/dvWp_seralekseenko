ead1328e06931f5eb1bbc3ba76bdc391fbb29c49
seralekseenko <seralekseenko@ukr.net>
2018-09-02 23:00:20 +0300

    выполнил пункт 5

--- make-app.mk
+++ make-app.mk
@@ -1,10 +1,13 @@
 USER = "$(shell id -u):$(shell id -g)"
 
 app:
-	docker-compose up
+	docker-compose up -d
 
 app-build:
 	docker-compose build
 
 app-bash:
 	docker-compose run --user=$(USER) app bash
+
+stop:
+	docker-compose stop
