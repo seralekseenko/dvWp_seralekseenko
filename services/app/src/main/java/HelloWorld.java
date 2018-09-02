e5e0928f03b31dabab4909199888344ebbad0b07
seralekseenko <seralekseenko@ukr.net>
2018-09-02 15:02:15 +0300

    подтянул зависимости. Улучшил вывод

--- services/app/src/main/java/HelloWorld.java
+++ services/app/src/main/java/HelloWorld.java
@@ -1,7 +1,14 @@
+import java.util.concurrent.atomic.AtomicInteger;
+
 import static spark.Spark.*;
 
 public class HelloWorld {
     public static void main(String[] args) {
-        get("/hello", (req, res) -> "###Hello World###\n");
+        AtomicInteger i = new AtomicInteger();
+        AtomicInteger j = new AtomicInteger();
+
+        get("/hello", (req, res) -> "###Hello World### count:" + (i.incrementAndGet()) + "\n");
+
+        get("/1", (req, res) -> "#Hello World1 count:" + (j.incrementAndGet()) + "\n");
     }
 }
